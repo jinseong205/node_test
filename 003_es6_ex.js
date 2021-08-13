958ff9a928530c4e2490f00402eb407626e57f4b
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-08-13 21:11:15 +0900

    Initial commit

---
+++ 003_es6_ex.js
@@ -0,0 +1,12 @@
+// es5
+function printHelloEs5(){
+    console.log('hello es5');
+}
+
+//es6
+const printHelloEs6 = ()=>{
+    console.log("hello es6");
+}
+
+printHelloEs5();
+printHelloEs6();
---
+++ 005_formatting_ex.js
@@ -0,0 +1,3 @@
+console.log('name : %s', 'jinseong');
+console.log('age : %d', 29); 
+console.log('pi : %d', 3.14);
